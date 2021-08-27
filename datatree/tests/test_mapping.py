e14f7a913bec48131c9ae32a68d23b0bda11310d
Thomas Nicholas <thomas.nicholas@columbia.edu>
2021-08-27 17:07:41 -0400

    cleaned up the mapping tests a bit

--- datatree/tests/test_mapping.py
+++ datatree/tests/test_mapping.py
@@ -83,45 +83,24 @@ class TestMapOverSubTree:
         ...
 
     def test_single_dt_arg(self):
-        dt = create_test_datatree()
+        dt = create_test_datatree(modify=lambda ds: 10.0 * ds)
 
         @map_over_subtree
         def times_ten(ds):
             return 10.0 * ds
 
         result_tree = times_ten(dt)
-
-        # TODO write an assert_tree_equal function
-        for (
-            result_node,
-            original_node,
-        ) in zip(result_tree.subtree, dt.subtree):
-            assert isinstance(result_node, DataTree)
-
-            if original_node.has_data:
-                assert_equal(result_node.ds, original_node.ds * 10.0)
-            else:
-                assert not result_node.has_data
+        assert_tree_equal(result_tree, dt)
 
     def test_single_dt_arg_plus_args_and_kwargs(self):
-        dt = create_test_datatree()
+        dt = create_test_datatree(modify=lambda ds: (10.0 * ds) + 2.0)
 
         @map_over_subtree
         def multiply_then_add(ds, times, add=0.0):
             return times * ds + add
 
         result_tree = multiply_then_add(dt, 10.0, add=2.0)
-
-        for (
-            result_node,
-            original_node,
-        ) in zip(result_tree.subtree, dt.subtree):
-            assert isinstance(result_node, DataTree)
-
-            if original_node.has_data:
-                assert_equal(result_node.ds, (original_node.ds * 10.0) + 2.0)
-            else:
-                assert not result_node.has_data
+        assert_tree_equal(result_tree, dt)
 
     def test_multiple_dt_args(self):
         ds = xr.Dataset({"a": ("x", [1, 2, 3])})
