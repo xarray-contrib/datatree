c24ca3faeae4fc35ee6c745bbcb279356964ef42
Thomas Nicholas <thomas.nicholas@columbia.edu>
2021-09-01 20:32:40 -0400

    checking types now works for trees with only one node

--- datatree/mapping.py
+++ datatree/mapping.py
@@ -2,7 +2,7 @@ import functools
 from itertools import repeat
 
 from anytree.iterators import LevelOrderIter
-from xarray import Dataset, DataArray
+from xarray import DataArray, Dataset
 
 from .treenode import TreeNode
 
@@ -44,9 +44,13 @@ def _check_isomorphic(subtree_a, subtree_b, require_names_equal=False):
     # TODO turn this into a public function called assert_isomorphic
 
     if not isinstance(subtree_a, TreeNode):
-        raise TypeError(f"Argument `subtree_a` is not a tree, it is of type {type(subtree_a)}")
+        raise TypeError(
+            f"Argument `subtree_a` is not a tree, it is of type {type(subtree_a)}"
+        )
     if not isinstance(subtree_b, TreeNode):
-        raise TypeError(f"Argument `subtree_b` is not a tree, it is of type {type(subtree_b)}")
+        raise TypeError(
+            f"Argument `subtree_b` is not a tree, it is of type {type(subtree_b)}"
+        )
 
     # Walking nodes in "level-order" fashion means walking down from the root breadth-first.
     # Checking by walking in this way implicitly assumes that the tree is an ordered tree (which it is so long as
@@ -56,21 +60,27 @@ def _check_isomorphic(subtree_a, subtree_b, require_names_equal=False):
 
         if require_names_equal:
             if node_a.name != node_b.name:
-                raise TreeIsomorphismError(f"Trees are not isomorphic because node '{path_a}' in the first tree has "
-                                           f"name '{node_a.name}', whereas its counterpart node '{path_b}' in the "
-                                           f"second tree has name '{node_b.name}'.")
+                raise TreeIsomorphismError(
+                    f"Trees are not isomorphic because node '{path_a}' in the first tree has "
+                    f"name '{node_a.name}', whereas its counterpart node '{path_b}' in the "
+                    f"second tree has name '{node_b.name}'."
+                )
 
         if node_a.has_data != node_b.has_data:
-            dat_a = 'no ' if not node_a.has_data else ''
-            dat_b = 'no ' if not node_b.has_data else ''
-            raise TreeIsomorphismError(f"Trees are not isomorphic because node '{path_a}' in the first tree has "
-                                       f"{dat_a}data, whereas its counterpart node '{path_b}' in the second tree "
-                                       f"has {dat_b}data.")
+            dat_a = "no " if not node_a.has_data else ""
+            dat_b = "no " if not node_b.has_data else ""
+            raise TreeIsomorphismError(
+                f"Trees are not isomorphic because node '{path_a}' in the first tree has "
+                f"{dat_a}data, whereas its counterpart node '{path_b}' in the second tree "
+                f"has {dat_b}data."
+            )
 
         if len(node_a.children) != len(node_b.children):
-            raise TreeIsomorphismError(f"Trees are not isomorphic because node '{path_a}' in the first tree has "
-                                       f"{len(node_a.children)} children, whereas its counterpart node '{path_b}' in "
-                                       f"the second tree has {len(node_b.children)} children.")
+            raise TreeIsomorphismError(
+                f"Trees are not isomorphic because node '{path_a}' in the first tree has "
+                f"{len(node_a.children)} children, whereas its counterpart node '{path_b}' in "
+                f"the second tree has {len(node_b.children)} children."
+            )
 
 
 def map_over_subtree(func):
@@ -98,7 +108,7 @@ def map_over_subtree(func):
         Positional arguments passed on to `func`. If DataTrees any data-containing nodes will be converted to Datasets \
         via .ds .
     **kwargs : Any
-        Keyword arguments passed on to `func`. If DataTrees any data-containing nodes will be converted to Datasets 
+        Keyword arguments passed on to `func`. If DataTrees any data-containing nodes will be converted to Datasets
         via .ds .
 
     Returns
@@ -135,58 +145,54 @@ def map_over_subtree(func):
             _check_isomorphic(first_tree, other_tree, require_names_equal=False)
 
         # Walk all trees simultaneously, applying func to all nodes that lie in same position in different trees
-        # We don't know which arguments are DataTrees so we zip all arguments together
-        # TODO : do we even need to tell repeat the length of the subtree?
+        # We don't know which arguments are DataTrees so we zip all arguments together as iterables
+        # Store tuples of results in a dict because we don't yet know how many trees we need to rebuild to return
         out_data_objects = {}
-        #size_tree = len(list(first_tree.subtree))
-        args_as_subtrees = [a.subtree if isinstance(a, DataTree) else repeat(a) for a in args]
-        n_args = len(args_as_subtrees)
-        kwargs_as_subtrees = {k: v.subtree if isinstance(v, DataTree) else repeat(v) for k, v in kwargs.items()}
-
-        #print(args_as_subtrees)
-        #print(kwargs_as_subtrees)
-        #print(list(kwargs_as_subtrees.values()))
-        # node_args_as_datasets, node_kwargs_as_datasets \
-        for node_of_first_tree, *all_node_args_as_subtrees in zip(first_tree.subtree, *args_as_subtrees, *list(kwargs_as_subtrees.values())):
-            # (outdated) node_args_as_datasets will now be in form tuple(Union[a.ds, a] for a in args)
-            # (outdated) so make node_kwargs_as_datasets into form dict(k: Union[v.ds, v] for k, v in kwargs)
-
-            print("node args")
-            #print(all_node_args_as_subtrees)
-            node_args_as_datasets = [a.ds if isinstance(a, DataTree) else a for a in all_node_args_as_subtrees[:n_args]]
-            print(node_args_as_datasets)
-            node_kwargs_as_datasets = dict(zip([k for k in kwargs_as_subtrees.keys()],
-                                               [v.ds if isinstance(v, DataTree) else v for v in all_node_args_as_subtrees[n_args:]]))
-            print(node_kwargs_as_datasets)
+        args_as_tree_length_iterables = [
+            a.subtree if isinstance(a, DataTree) else repeat(a) for a in args
+        ]
+        n_args = len(args_as_tree_length_iterables)
+        kwargs_as_tree_length_iterables = {
+            k: v.subtree if isinstance(v, DataTree) else repeat(v)
+            for k, v in kwargs.items()
+        }
+        for node_of_first_tree, *all_node_args in zip(
+            first_tree.subtree,
+            *args_as_tree_length_iterables,
+            *list(kwargs_as_tree_length_iterables.values()),
+        ):
+            node_args_as_datasets = [
+                a.ds if isinstance(a, DataTree) else a for a in all_node_args[:n_args]
+            ]
+            node_kwargs_as_datasets = dict(
+                zip(
+                    [k for k in kwargs_as_tree_length_iterables.keys()],
+                    [
+                        v.ds if isinstance(v, DataTree) else v
+                        for v in all_node_args[n_args:]
+                    ],
+                )
+            )
 
             # Now we can call func on the data in this particular set of corresponding nodes
-            results = func(*node_args_as_datasets, **node_kwargs_as_datasets) if node_of_first_tree.has_data else None
-            print(results)
+            results = (
+                func(*node_args_as_datasets, **node_kwargs_as_datasets)
+                if node_of_first_tree.has_data
+                else None
+            )
 
-            # Store tuples of results in a dict because we don't yet know how many trees we need to rebuild to return
-            # TODO implement mapping over multiple trees inplace using if conditions from here on?
-            # TODO make a proper relative_path method
-            #path = node_first_tree.pathstr  # .replace(first_tree.pathstr, "")
-            relative_path = node_of_first_tree.pathstr#.replace(first_tree.pathstr, "")
-            #print(relative_path)
-            out_data_objects[relative_path] = results
+            # TODO implement mapping over multiple trees in-place using if conditions from here on?
+            out_data_objects[node_of_first_tree.pathstr] = results
 
-        # Find out how many return values we had
-        num_return_values = _check_return_values(out_data_objects)
-        print(out_data_objects)
-        print("number of return values")
-        print(num_return_values)
+        # Find out how many return values we received
+        num_return_values = _check_all_return_values(out_data_objects)
 
-        # Reconstruct potentially multiple subtrees from the dict of results
-        # Fill in all nodes of all result trees
+        # Reconstruct 1+ subtrees from the dict of results, by filling in all nodes of all result trees
         result_trees = []
         for i in range(num_return_values):
             out_tree_contents = {}
             for n in first_tree.subtree:
-                p = n.pathstr#.replace(first_tree.pathstr, "")
-                print("results to add")
-                print(i)
-                print(out_data_objects[p])
+                p = n.pathstr
                 if p in out_data_objects.keys():
                     if isinstance(out_data_objects[p], tuple):
                         output_node_data = out_data_objects[p][i]
@@ -196,11 +202,8 @@ def map_over_subtree(func):
                     output_node_data = None
                 out_tree_contents[p] = output_node_data
 
-            # TODO: document how names are just taken from first dt passed
-            # TODO: Possible bug - what happens if another tree argument does not have root named the same way?
-            #print(out_tree_contents)
+            # TODO: document how names are just taken from first DataTree passed
             new_tree = DataTree(name=first_tree.name, data_objects=out_tree_contents)
-            #print(new_tree)
             result_trees.append(new_tree)
 
         # If only one result then don't wrap it in a tuple
@@ -212,37 +215,60 @@ def map_over_subtree(func):
     return _map_over_subtree
 
 
-def _check_return_values(returned_objects):
+def _check_single_set_return_values(path_to_node, obj):
+    """Check types returned from single evaluation of func, and return number of return values received from func."""
+    if isinstance(obj, (Dataset, DataArray)):
+        return 1
+    elif isinstance(obj, tuple):
+        for r in obj:
+            if not isinstance(r, (Dataset, DataArray)):
+                raise TypeError(
+                    f"One of the results of calling func on datasets on the nodes at position {path_to_node} is "
+                    f"of type {type(r)}, not Dataset or DataArray."
+                )
+        return len(obj)
+    else:
+        raise TypeError(
+            f"The result of calling func on the node at position {path_to_node} is of type {type(obj)}, not "
+            f"Dataset or DataArray, nor a tuple of such types."
+        )
+
+
+def _check_all_return_values(returned_objects):
     """Walk through all values returned by mapping func over subtrees, raising on any invalid or inconsistent types."""
-    print(returned_objects)
 
     if all(r is None for r in returned_objects.values()):
-        raise TypeError("Called supplied function on all nodes but found a return value of None for"
-                        "all of them.")
+        raise TypeError(
+            "Called supplied function on all nodes but found a return value of None for"
+            "all of them."
+        )
 
-    result_data_objects = [(p, r) for p, r in returned_objects.items() if r is not None]
+    result_data_objects = [
+        (path_to_node, r)
+        for path_to_node, r in returned_objects.items()
+        if r is not None
+    ]
 
-    prev_path, prev_obj = result_data_objects[0]
-    prev_num_return_values, num_return_values = None, None
-    for p, obj in result_data_objects[1:]:
-        if isinstance(obj, (Dataset, DataArray)):
-            num_return_values = 1
-        elif isinstance(obj, tuple):
-            for r in obj:
-                if not isinstance(r, (Dataset, DataArray)):
-                    raise TypeError(f"One of the results of calling func on datasets on the nodes at position {p} is "
-                                    f"of type {type(r)}, not Dataset or DataArray.")
+    if len(result_data_objects) == 1:
+        # Only one node in the tree: no need to check consistency of results between nodes
+        path_to_node, result = result_data_objects[0]
+        num_return_values = _check_single_set_return_values(path_to_node, result)
+    else:
+        prev_path, _ = result_data_objects[0]
+        prev_num_return_values, num_return_values = None, None
+        for path_to_node, obj in result_data_objects[1:]:
+            num_return_values = _check_single_set_return_values(path_to_node, obj)
 
-            num_return_values = len(obj)
-        else:
-            raise TypeError(f"The result of calling func on the node at position {p} is of type {type(obj)}, not "
-                            f"Dataset or DataArray, nor a tuple of such types.")
+            if (
+                num_return_values != prev_num_return_values
+                and prev_num_return_values is not None
+            ):
+                raise TypeError(
+                    f"Calling func on the nodes at position {path_to_node} returns {num_return_values} separate return "
+                    f"values, whereas calling func on the nodes at position {prev_path} instead returns "
+                    f"{prev_num_return_values} separate return values."
+                )
 
-        if num_return_values != prev_num_return_values and prev_num_return_values is not None:
-            raise TypeError(f"Calling func on the nodes at position {p} returns {num_return_values} separate return "
-                            f"values, whereas calling func on the nodes at position {prev_path} instead returns "
-                            f"{prev_num_return_values} separate return values.")
-
-        prev_path, prev_obj, prev_num_return_values = p, obj, num_return_values
+            prev_path, prev_num_return_values = path_to_node, num_return_values
 
     return num_return_values
--- datatree/tests/test_mapping.py
+++ datatree/tests/test_mapping.py
@@ -1,13 +1,10 @@
 import pytest
 import xarray as xr
-from xarray.testing import assert_equal
-
-from datatree.treenode import TreeNode
-from datatree.datatree import DataNode, DataTree
-from datatree.mapping import _check_isomorphic, TreeIsomorphismError, map_over_subtree
-
 from test_datatree import assert_tree_equal, create_test_datatree
 
+from datatree.datatree import DataTree
+from datatree.mapping import TreeIsomorphismError, _check_isomorphic, map_over_subtree
+from datatree.treenode import TreeNode
 
 empty = xr.Dataset()
 
@@ -15,59 +12,79 @@ empty = xr.Dataset()
 class TestCheckTreesIsomorphic:
     def test_not_a_tree(self):
         with pytest.raises(TypeError, match="not a tree"):
-            _check_isomorphic('s', 1)
+            _check_isomorphic("s", 1)
 
     def test_different_widths(self):
-        dt1 = DataTree(data_objects={'a': empty})
-        dt2 = DataTree(data_objects={'a': empty, 'b': empty})
-        expected_err_str = "'root' in the first tree has 1 children, whereas its counterpart node 'root' in the " \
-                           "second tree has 2 children"
+        dt1 = DataTree(data_objects={"a": empty})
+        dt2 = DataTree(data_objects={"a": empty, "b": empty})
+        expected_err_str = (
+            "'root' in the first tree has 1 children, whereas its counterpart node 'root' in the "
+            "second tree has 2 children"
+        )
         with pytest.raises(TreeIsomorphismError, match=expected_err_str):
             _check_isomorphic(dt1, dt2)
 
     def test_different_heights(self):
-        dt1 = DataTree(data_objects={'a': empty})
-        dt2 = DataTree(data_objects={'a': empty, 'a/b': empty})
-        expected_err_str = "'root/a' in the first tree has 0 children, whereas its counterpart node 'root/a' in the " \
-                           "second tree has 1 children"
+        dt1 = DataTree(data_objects={"a": empty})
+        dt2 = DataTree(data_objects={"a": empty, "a/b": empty})
+        expected_err_str = (
+            "'root/a' in the first tree has 0 children, whereas its counterpart node 'root/a' in the "
+            "second tree has 1 children"
+        )
         with pytest.raises(TreeIsomorphismError, match=expected_err_str):
             _check_isomorphic(dt1, dt2)
 
     def test_only_one_has_data(self):
-        dt1 = DataTree(data_objects={'a': xr.Dataset({'a': 0})})
-        dt2 = DataTree(data_objects={'a': None})
-        expected_err_str = "'root/a' in the first tree has data, whereas its counterpart node 'root/a' in the " \
-                           "second tree has no data"
+        dt1 = DataTree(data_objects={"a": xr.Dataset({"a": 0})})
+        dt2 = DataTree(data_objects={"a": None})
+        expected_err_str = (
+            "'root/a' in the first tree has data, whereas its counterpart node 'root/a' in the "
+            "second tree has no data"
+        )
         with pytest.raises(TreeIsomorphismError, match=expected_err_str):
             _check_isomorphic(dt1, dt2)
 
     def test_names_different(self):
-        dt1 = DataTree(data_objects={'a': xr.Dataset()})
-        dt2 = DataTree(data_objects={'b': empty})
-        expected_err_str = "'root/a' in the first tree has name 'a', whereas its counterpart node 'root/b' in the " \
-                           "second tree has name 'b'"
+        dt1 = DataTree(data_objects={"a": xr.Dataset()})
+        dt2 = DataTree(data_objects={"b": empty})
+        expected_err_str = (
+            "'root/a' in the first tree has name 'a', whereas its counterpart node 'root/b' in the "
+            "second tree has name 'b'"
+        )
         with pytest.raises(TreeIsomorphismError, match=expected_err_str):
             _check_isomorphic(dt1, dt2, require_names_equal=True)
 
     def test_isomorphic_names_equal(self):
-        dt1 = DataTree(data_objects={'a': empty, 'b': empty, 'b/c': empty, 'b/d': empty})
-        dt2 = DataTree(data_objects={'a': empty, 'b': empty, 'b/c': empty, 'b/d': empty})
+        dt1 = DataTree(
+            data_objects={"a": empty, "b": empty, "b/c": empty, "b/d": empty}
+        )
+        dt2 = DataTree(
+            data_objects={"a": empty, "b": empty, "b/c": empty, "b/d": empty}
+        )
         _check_isomorphic(dt1, dt2, require_names_equal=True)
 
     def test_isomorphic_ordering(self):
-        dt1 = DataTree(data_objects={'a': empty, 'b': empty, 'b/d': empty, 'b/c': empty})
-        dt2 = DataTree(data_objects={'a': empty, 'b': empty, 'b/c': empty, 'b/d': empty})
+        dt1 = DataTree(
+            data_objects={"a": empty, "b": empty, "b/d": empty, "b/c": empty}
+        )
+        dt2 = DataTree(
+            data_objects={"a": empty, "b": empty, "b/c": empty, "b/d": empty}
+        )
         _check_isomorphic(dt1, dt2, require_names_equal=False)
 
     def test_isomorphic_names_not_equal(self):
-        dt1 = DataTree(data_objects={'a': empty, 'b': empty, 'b/c': empty, 'b/d': empty})
-        dt2 = DataTree(data_objects={'A': empty, 'B': empty, 'B/C': empty, 'B/D': empty})
+        dt1 = DataTree(
+            data_objects={"a": empty, "b": empty, "b/c": empty, "b/d": empty}
+        )
+        dt2 = DataTree(
+            data_objects={"A": empty, "B": empty, "B/C": empty, "B/D": empty}
+        )
         _check_isomorphic(dt1, dt2)
 
     def test_not_isomorphic_complex_tree(self):
         dt1 = create_test_datatree()
         dt2 = create_test_datatree()
-        dt2.set_node('set1/set2', TreeNode('set3'))
+        dt2.set_node("set1/set2", TreeNode("set3"))
         with pytest.raises(TreeIsomorphismError, match="root/set1/set2"):
             _check_isomorphic(dt1, dt2)
 
@@ -84,7 +101,7 @@ class TestMapOverSubTree:
     def test_not_isomorphic(self):
         dt1 = create_test_datatree()
         dt2 = create_test_datatree()
-        dt2['set4'] = None
+        dt2["set4"] = None
 
         @map_over_subtree
         def times_ten(ds1, ds2):
@@ -203,7 +220,9 @@ class TestMapOverSubTree:
         def times_ten(ds):
             return 10.0 * ds
 
-        with pytest.raises(TypeError, match="takes 1 positional argument but 2 were given"):
+        with pytest.raises(
+            TypeError, match="takes 1 positional argument but 2 were given"
+        ):
             times_ten(dt, dt)
 
     def test_map_single_dataset_against_whole_tree(self):
@@ -213,7 +232,7 @@ class TestMapOverSubTree:
         def nodewise_merge(node_ds, fixed_ds):
             return xr.merge([node_ds, fixed_ds])
 
-        other_ds = xr.Dataset({'z': ('z', [0])})
+        other_ds = xr.Dataset({"z": ("z", [0])})
         expected = create_test_datatree(modify=lambda ds: xr.merge([ds, other_ds]))
         result_tree = nodewise_merge(dt, other_ds)
         assert_tree_equal(result_tree, expected)
