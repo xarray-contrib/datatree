34f630d39716130a3815b8ca1cf3d19f92ff3f11
Thomas Nicholas <thomas.nicholas@columbia.edu>
2021-12-16 14:42:54 -0500

    added tests of tree diff formatting

--- datatree/formatting.py
+++ datatree/formatting.py
@@ -6,15 +6,19 @@ from .mapping import diff_treestructure
 def diff_nodewise_summary(a, b, compat):
     """Iterates over all corresponding nodes, recording differences between data at each location."""
 
+    compat_str = _compat_to_str(compat)
+
     summary = []
     for node_a, node_b in zip(a.subtree, b.subtree):
         a_ds, b_ds = node_a.ds, node_b.ds
 
         if not a_ds._all_compat(b_ds, compat):
             path = node_a.pathstr
+            dataset_diff = diff_dataset_repr(a_ds, b_ds, compat_str)
+            data_diff = "\n".join(dataset_diff.split("\n", 1)[1:])
+
             nodediff = (
-                f"Data in nodes at position {path} do not match:\n"
-                f"{diff_dataset_repr(a, b, compat)}"
+                f"\nData in nodes at position '{path}' do not match:" f"{data_diff}"
             )
             summary.append(nodediff)
 
@@ -28,16 +32,15 @@ def diff_tree_repr(a, b, compat):
 
     # TODO check root parents?
 
-    treestructure_diff = diff_treestructure(
-        a, b, True if compat == "identical" else False
-    )
+    strict_names = True if compat in ["equals", "identical"] else False
+    treestructure_diff = diff_treestructure(a, b, strict_names)
 
     # If the trees structures are different there is no point comparing each node
     # TODO we could show any differences in nodes up to the first place that structure differs?
     if treestructure_diff or compat == "isomorphic":
-        summary.append(treestructure_diff)
+        summary.append("\n" + treestructure_diff)
     else:
         nodewise_diff = diff_nodewise_summary(a, b, compat)
-        summary.append(nodewise_diff)
+        summary.append("\n" + nodewise_diff)
 
     return "\n".join(summary)
