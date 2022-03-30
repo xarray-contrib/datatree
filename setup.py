3f5f12b51ce586ab40020c17e1b898d46a93ed21
lsetiawan <landungs@uw.edu>
2022-03-30 11:09:35 -0700

    Fix missing comma in classifiers

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
