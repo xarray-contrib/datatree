4c45128c8b378783aae33e5b82b77233b4fb6246
Thomas Nicholas <thomas.nicholas@columbia.edu>
2022-04-23 17:33:59 -0400

    refactoring

--- datatree/formatting_html.py
+++ datatree/formatting_html.py
@@ -1,30 +1,28 @@
-from html import escape
 from functools import partial
-from typing import Mapping
+from html import escape
+from typing import TYPE_CHECKING, Mapping
 
 from xarray.core.formatting_html import (
+    _mapping_section,
     _obj_repr,
     attr_section,
     coord_section,
     datavar_section,
     dim_section,
-    _mapping_section,
 )
-
 from xarray.core.options import OPTIONS
 
-OPTIONS['display_expand_groups'] = "default"
+if TYPE_CHECKING:
+    from .datatree import DataTree
+
+OPTIONS["display_expand_groups"] = "default"
 
 
-def summarize_children(children: Mapping[str, "DataTree"]) -> str:
-    children_li_elements = [
-        f"<ul class='xr-sections'>{group_repr(n, c)}</ul>"
-        for n, c in children.items()
-    ]
-
+def summarize_children(children: Mapping[str, DataTree]) -> str:
     children_li = "".join(
-        children_li_elements
+        f"<ul class='xr-sections'>{node_repr(n, c)}</ul>" for n, c in children.items()
     )
+
     return (
         "<ul class='xr-sections'>"
         f"<div style='padding-left:2rem;'>{children_li}<br></div>"
@@ -41,8 +39,8 @@ children_section = partial(
 )
 
 
-def group_repr(group_name: str, dt: "DataTree") -> str:
-    header_components = [f"<div class='xr-obj-type'>{escape(group_name)}</div>"]
+def node_repr(group_title: str, dt: DataTree) -> str:
+    header_components = [f"<div class='xr-obj-type'>{escape(group_title)}</div>"]
 
     ds = dt.ds
     children = {c.name: c for c in dt.children}
@@ -58,20 +56,6 @@ def group_repr(group_name: str, dt: "DataTree") -> str:
     return _obj_repr(ds, header_components, sections)
 
 
-def datatree_repr(dt: "DataTree") -> str:
+def datatree_repr(dt: DataTree) -> str:
     obj_type = f"datatree.{type(dt).__name__}"
-
-    header_components = [f"<div class='xr-obj-type'>{escape(obj_type)}</div>"]
-
-    ds = dt.ds
-    children = {c.name: c for c in dt.children}
-
-    sections = [
-        children_section(children),
-        dim_section(ds),
-        coord_section(ds.coords),
-        datavar_section(ds.data_vars),
-        attr_section(ds.attrs),
-    ]
-
-    return _obj_repr(ds, header_components, sections)
+    return node_repr(obj_type, dt)
