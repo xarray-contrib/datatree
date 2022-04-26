ae9db9ec17845dd397848be616970aea469f1be3
Thomas Nicholas <thomas.nicholas@columbia.edu>
2022-04-26 16:32:39 -0400

    black

--- datatree/render.py
+++ datatree/render.py
@@ -8,7 +8,6 @@ Row = collections.namedtuple("Row", ("pre", "fill", "node"))
 
 
 class AbstractStyle(object):
-
     def __init__(self, vertical, cont, end):
         """
         Tree Render Style.
@@ -21,21 +20,22 @@ class AbstractStyle(object):
         self.vertical = vertical
         self.cont = cont
         self.end = end
-        assert len(cont) == len(vertical) == len(end), f"'{vertical}', '{cont}' and '{end}' need to have equal length"
+        assert (
+            len(cont) == len(vertical) == len(end)
+        ), f"'{vertical}', '{cont}' and '{end}' need to have equal length"
 
     @property
     def empty(self):
         """Empty string as placeholder."""
-        return ' ' * len(self.end)
+        return " " * len(self.end)
 
     def __repr__(self):
         return f"{self.__class__.__name__}()"
 
 
 class ContStyle(AbstractStyle):
-
     def __init__(self):
-        u"""
+        """
         Continued style, without gaps.
 
         >>> from anytree import Node, RenderTree
@@ -52,15 +52,14 @@ class ContStyle(AbstractStyle):
         │   └── Node('/root/sub0/sub0A')
         └── Node('/root/sub1')
         """
-        super(ContStyle, self).__init__(u'\u2502   ',
-                                        u'\u251c\u2500\u2500 ',
-                                        u'\u2514\u2500\u2500 ')
+        super(ContStyle, self).__init__(
+            "\u2502   ", "\u251c\u2500\u2500 ", "\u2514\u2500\u2500 "
+        )
 
 
 class RenderTree(object):
-
     def __init__(self, node, style=ContStyle(), childiter=list, maxlevel=None):
-        u"""
+        """
         Render tree starting at `node`.
         Keyword Args:
             style (AbstractStyle): Render Style.
@@ -83,6 +82,7 @@ class RenderTree(object):
         Simple one line:
         >>> for pre, _, node in RenderTree(root):
         ...     print("%s%s" % (pre, node.name))
+        ...
         root
         ├── sub0
         │   ├── sub0B
@@ -93,6 +93,7 @@ class RenderTree(object):
         ...     print("%s%s" % (pre, node.lines[0]))
         ...     for line in node.lines[1:]:
         ...         print("%s%s" % (fill, line))
+        ...
         c0fe
         c0de
         ├── ha
@@ -112,6 +113,7 @@ class RenderTree(object):
         same level. An reversed order can be achived by using `reversed`.
         >>> for row in RenderTree(root, childiter=reversed):
         ...     print("%s%s" % (row.pre, row.node.name))
+        ...
         root
         ├── sub1
         └── sub0
@@ -120,8 +122,10 @@ class RenderTree(object):
         Or writing your own sort function:
         >>> def mysort(items):
         ...     return sorted(items, key=lambda item: item.name)
+        ...
         >>> for row in RenderTree(root, childiter=mysort):
         ...     print("%s%s" % (row.pre, row.node.name))
+        ...
         root
         ├── sub0
         │   ├── sub0A
@@ -170,19 +174,21 @@ class RenderTree(object):
         if children and (self.maxlevel is None or level < self.maxlevel):
             children = self.childiter(children)
             for child, is_last in _is_last(children):
-                for grandchild in self.__next(child, continues + (not is_last, ), level=level):
+                for grandchild in self.__next(
+                    child, continues + (not is_last,), level=level
+                ):
                     yield grandchild
 
     @staticmethod
     def __item(node, continues, style):
         if not continues:
-            return Row(u'', u'', node)
+            return Row("", "", node)
         else:
             items = [style.vertical if cont else style.empty for cont in continues]
-            indent = ''.join(items[:-1])
+            indent = "".join(items[:-1])
             branch = style.cont if continues[-1] else style.end
             pre = indent + branch
-            fill = ''.join(items)
+            fill = "".join(items)
             return Row(pre, fill, node)
 
     def __str__(self):
@@ -191,13 +197,15 @@ class RenderTree(object):
 
     def __repr__(self):
         classname = self.__class__.__name__
-        args = [repr(self.node),
-                "style=%s" % repr(self.style),
-                "childiter=%s" % repr(self.childiter)]
+        args = [
+            repr(self.node),
+            "style=%s" % repr(self.style),
+            "childiter=%s" % repr(self.childiter),
+        ]
         return "%s(%s)" % (classname, ", ".join(args))
 
     def by_attr(self, attrname="name"):
-        u"""
+        """
         Return rendered tree with node attribute `attrname`.
         >>> from anytree import AnyNode, RenderTree
         >>> root = AnyNode(id="root")
@@ -209,7 +217,7 @@ class RenderTree(object):
         >>> s1b = AnyNode(id="sub1B", parent=s1, bar=8)
         >>> s1c = AnyNode(id="sub1C", parent=s1)
         >>> s1ca = AnyNode(id="sub1Ca", parent=s1c)
-        >>> print(RenderTree(root).by_attr('id'))
+        >>> print(RenderTree(root).by_attr("id"))
         root
         ├── sub0
         │   ├── sub0B
@@ -220,16 +228,21 @@ class RenderTree(object):
             └── sub1C
                 └── sub1Ca
         """
+
         def get():
             for pre, fill, node in self:
-                attr = attrname(node) if callable(attrname) else getattr(node, attrname, "")
+                attr = (
+                    attrname(node)
+                    if callable(attrname)
+                    else getattr(node, attrname, "")
+                )
                 if isinstance(attr, (list, tuple)):
                     lines = attr
                 else:
                     lines = str(attr).split("\n")
-                yield u"%s%s" % (pre, lines[0])
+                yield "%s%s" % (pre, lines[0])
                 for line in lines[1:]:
-                    yield u"%s%s" % (fill, line)
+                    yield "%s%s" % (fill, line)
 
         return "\n".join(get())
 
