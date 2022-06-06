102ec91a4c35e3cb9b27f6d76a6564c8ea70c92d
Benjamin Woods <ben@bjqw.me>
2022-06-06 21:50:12 +0100

    Modify HTML repr to add "anytree" style
    * Instead of indenting children, wrap each node_repr into a three column 
    CSS grid
    * Column 1 has right border, column 2 has 2 rows, one with bottom 
    border, column 3 contains the node_repr
    * Set the height of column 1 depending on whether it is the end of a 
    list of nodes in the tree or not

--- datatree/formatting_html.py
+++ datatree/formatting_html.py
@@ -14,17 +14,18 @@ from xarray.core.options import OPTIONS
 
 OPTIONS["display_expand_groups"] = "default"
 
-
 def summarize_children(children: Mapping[str, Any]) -> str:
-    children_li = "".join(
-        f"<ul class='xr-sections'>{node_repr(n, c)}</ul>" for n, c in children.items()
+    children_html = "".join(
+        _wrapped_node_repr(n, c, end=False)                          # Long lines
+        if i < len(children)-1 else _wrapped_node_repr(n, c, end=True) # Short lines
+        for i, (n, c) in enumerate(children.items())
     )
 
-    return (
-        "<ul class='xr-sections'>"
-        f"<div style='padding-left:2rem;'>{children_li}<br></div>"
-        "</ul>"
-    )
+    return "".join([
+        "<div style='display: inline-grid; grid-template-columns: 100%'>",
+            children_html,
+        "</div>"
+    ])
 
 
 children_section = partial(
@@ -34,7 +35,6 @@ children_section = partial(
     max_items_collapse=1,
     expand_option_name="display_expand_groups",
 )
-
 
 def node_repr(group_title: str, dt: Any) -> str:
     header_components = [f"<div class='xr-obj-type'>{escape(group_title)}</div>"]
@@ -50,6 +50,42 @@ def node_repr(group_title: str, dt: Any) -> str:
     ]
 
     return _obj_repr(ds, header_components, sections)
+
+def _wrapped_node_repr(*args, end=False, **kwargs):
+    # Get node repr
+    r = node_repr(*args, **kwargs)
+
+    # height of line
+    end = bool(end)
+    height = "100%" if end is False else "1.2em";
+    return "".join([
+        "<div style='display: inline-grid;'>",
+            "<div style='",
+                "grid-column-start: 1;",
+                "border-right: 0.2em solid;",
+                "border-color: var(--xr-border-color);",
+                f"height: {height};",
+                "width: 0px;",
+            "'>",
+            "</div>",
+            "<div style='",
+                "grid-column-start: 2;",
+                "grid-row-start: 1;",
+                "height: 1em;",
+                "width: 20px;",
+                "border-bottom: 0.2em solid;",
+                "border-color: var(--xr-border-color);",
+            "'>",
+            "</div>",
+            "<div style='",
+                "grid-column-start: 3;",
+            "'>",
+                "<ul class='xr-sections'>",
+                    r,
+                "</ul>"
+            "</div>",
+        "</div>",
+    ])
 
 
 def datatree_repr(dt: Any) -> str:
